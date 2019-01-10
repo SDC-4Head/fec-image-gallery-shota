4bff5fed49cc8223a3636343716c2252df4196d1
Shota Sugawara <shrikethrush@gmail.com>
2019-01-09 17:34:01 -0800

    CRUD implement done

--- server/index.js
+++ server/index.js
@@ -3,15 +3,46 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const PORT = 1337;
-const model = require('../db/index');
+const mongodb = require('../db/index');
+const cassandra = require('../db/cassandra')
 
 app.use('/rooms/:id', express.static('./client/dist'));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.get('/rooms/:id/photos', (req, res) => {
-  var id = req.params.id;
-  model.getPhotosById(id, (err, response) => {
+  cassandra.getPhotos(req.params.id, (err, response) => {
+    if (err) {
+      res.status(501).send();
+    } else {
+      res.end(JSON.stringify(response));
+    }
+  });
+});
+
+app.post('/rooms/:id/photos', (req, res) => {
+  cassandra.insertPhotos(req.params.id, req.params.url, req.params.caption, (err, response) => {
+    if (err) {
+      res.status(501).send();
+    } else {
+      res.end(JSON.stringify(response));
+    }
+  });
+});
+
+app.put('/rooms/:id/photos', (req, res) => {
+
+  cassandra.updatePhotos(req.params.id, req.params.url, req.params.caption, (err, response) => {
+    if (err) {
+      res.status(501).send();
+    } else {
+      res.end(JSON.stringify(response));
+    }
+  });
+});
+
+app.delete('/rooms/:id/photos', (req, res) => {
+  cassandra.deletePhotos(req.params.id, (err, response) => {
     if (err) {
       res.status(501).send();
     } else {
